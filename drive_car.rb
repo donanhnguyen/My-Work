76aecd97119edcc9f60273a708dcbb9201823cec
Don Nguyen <thedonwind69@gmail.com>
2017-09-01 03:22:26 +0000

    Updating drive_car game

--- drive_car.rb
+++ drive_car.rb
@@ -22,7 +22,13 @@
 
 # Code Scenario:
 
-# In this example we will create an instance of Camaro and tell it to drive, we will assume it’s moving in a straight line and there are no other driving factors.  The car will accelerate until it hits its max speed.  It is required that drive will call accelerate. It is required accelerate will increment the current speed by 1.  Once the Camaro reaches max speed it should stop accelerating and print that it hit the cars max speed.  The execution of drive should then also stop.
+# In this example we will create an instance of Camaro and tell it to drive,
+# we will assume it’s moving in a straight line and there are no other
+# driving factors.  The car will accelerate until it hits its max speed.
+# It is required that drive will call accelerate. It is required accelerate
+# will increment the current speed by 1.  Once the Camaro reaches max speed
+# it should stop accelerating and print that it hit the cars max speed.  
+# The execution of drive should then also stop.
 
 class Car
   

f536df09e7e5e661c43721c01890c9fcd848931d
Don Nguyen <thedonwind69@gmail.com>
2017-09-01 03:20:57 +0000

    Updating drive_car game

--- drive_car.rb
+++ drive_car.rb
@@ -1,3 +1,29 @@
+# Code Scenario Requirements:
+
+# Base Car Class
+
+# + current speed (property) – default value 0
+
+# + accelerate (method)
+
+# + drive (method)
+
+# + brand (property) - default value ‘unknown’
+
+# + max speed (property) - default value 0
+
+# Camaro Car Class
+
+# + Inherits Base Car
+
+# + brand (property) - default value ‘Chevy’
+
+# + max speed (property) – default value 200
+
+# Code Scenario:
+
+# In this example we will create an instance of Camaro and tell it to drive, we will assume it’s moving in a straight line and there are no other driving factors.  The car will accelerate until it hits its max speed.  It is required that drive will call accelerate. It is required accelerate will increment the current speed by 1.  Once the Camaro reaches max speed it should stop accelerating and print that it hit the cars max speed.  The execution of drive should then also stop.
+
 class Car
   
   attr_accessor :current_speed, :max_speed
@@ -26,8 +52,10 @@ class Car
   end
 end
 
+if $PROGRAM_NAME == __FILE__
 puts "What car do you want to drive?"
 name = gets.chomp.upcase
 puts "Your car is a #{name}."
 name = Car.new
 name.drive
+end

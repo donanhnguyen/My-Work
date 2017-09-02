d63204c973da15b63fdad3c529f93f4c96c47336
Don Nguyen <thedonwind69@gmail.com>
2017-09-02 06:02:08 +0000

    Most common string problem

---
+++ most_common_string.rb
@@ -0,0 +1,29 @@
+
+
+def most_common_string(sentence)
+  word_tracker = {}
+  split_sentence = sentence.downcase.split(" ")
+  split_sentence.each do |word|
+    if word_tracker.include? word
+      word_tracker[word] += 1
+    else
+      word_tracker[word] = 1
+    end
+  end
+  
+  highest_count = 0
+  mcs = nil
+  word_tracker.keys.each do |word|
+    if word_tracker[word] > highest_count
+      highest_count = word_tracker[word]
+      mcs = word
+    end
+  end
+  if word_tracker.values.all? {|value| value == 1}
+    puts "There is no most common string."
+  else
+  puts "The most common string is \"#{mcs}\"."
+ end
+end
+
+puts most_common_string("The cat in the hat bought two other cats and the cat now has two other cats")

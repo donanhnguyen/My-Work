7fe7e8990a769a9583d6e25598bd96d7d6b01e70
Don Nguyen <thedonwind69@gmail.com>
2018-01-07 22:50:06 -0800

    adding some javascript to my_website

---
+++ my_website/js/messages.js
@@ -0,0 +1,52 @@
+$( () => {
+
+  
+    const $profilePic = $("#profile-pic");
+
+    $profilePic.on("click", function(event) {
+
+        const $this = $(this);
+        console.log($this);
+        $this.toggleClass("expand-pic");
+
+    })
+
+    const clearForm = function () {
+        $("#message-author").val("");
+        $("#message-content").val("");
+    }
+
+
+    const submitMessage = function (event) {
+        event.preventDefault();
+
+        const $newMessage = $("<p>");
+        const $messageAuthor = $("#message-author");
+        const $messageContent = $("#message-content");
+
+        $newMessage.text($messageAuthor.val() + " wrote: " + $messageContent.val());
+        $newMessage.addClass("new-message");
+
+        if ($messageAuthor.val() === "" || $messageContent.val() === "") {
+           alert("You didn't fill out all the info...");
+        } else {
+            $("#messages-list").append($newMessage); 
+            console.log($newMessage.text());
+        }
+
+        clearForm();
+
+    }
+
+    $("#message-form").on("submit", submitMessage);
+
+
+
+  
+
+
+
+
+
+
+})
